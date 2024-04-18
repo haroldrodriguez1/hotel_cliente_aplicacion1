f494fb4c123d8c32263f7bea9c74cf1a0ccd74fe
haroldrodriguez1 <haroldwinstonr2001@gmail.com>
2024-04-17 21:21:28 -0600

    mm

--- lib/pantallas/pantalla1.dart
+++ lib/pantallas/pantalla1.dart
@@ -35,192 +35,240 @@ bool showError = false;
         
         
       ),
-      body : SingleChildScrollView( child:
-      SafeArea (
-        child: Padding(
-          padding: const EdgeInsets.all(15.0),
-          child: Column(
-           
-          children: [
-            Row( 
-              mainAxisAlignment: MainAxisAlignment.center,
+      body : Stack(
+        children: [
+          Image.asset(
+          'assets/images/8316277.jpg',
+          fit: BoxFit.cover,
+          width: double.infinity,
+          height: double.infinity,
+        ),
+          SingleChildScrollView( child:
+          SafeArea (
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Column(
+               
               children: [
-            Icon( Icons.hotel_class),
-            Icon( Icons.hotel_class),
-            Icon( Icons.hotel_class),
-            Icon( Icons.hotel_class),
-            Icon( Icons.hotel_class),
-
-              ],
-            ),
-          Text(
-            "Inserte Datos", 
-            style: TextStyle(fontSize: 22,fontFamily:"CourrierPrime"),
-            ),
+                Row( 
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                Icon( Icons.hotel_class),
+                Icon( Icons.hotel_class),
+                Icon( Icons.hotel_class),
+                Icon( Icons.hotel_class),
+                Icon( Icons.hotel_class),
           
-            SizedBox(height: 50,
-            ),
-            TextField(
-              textAlign: TextAlign.center,
-              controller: fnamecontroller,
-              decoration: InputDecoration(labelText: "Nombre de Usuario"),
-            ),
-         Stack(
-        alignment: Alignment.centerRight,  
-        children: [  
-         TextField(
-            textAlign: TextAlign.center,
-            controller: contracontroller,
-            obscureText: obscureText,
-            decoration: InputDecoration(
-              labelText: "Contraseña",
-              errorText: showError ? "Contraseña o usuario incorrectos" : null,
-            ),
-            
-            
-          ),IconButton(
-      onPressed: () {
-        setState(() {
-          obscureText = !obscureText;
-        });
-      },
-      icon: Icon(
-        obscureText ? Icons.visibility_off : Icons.visibility,
-        color: Colors.grey,
-      ),
-    ),],),
-            SizedBox(height: 10,),
-            
-                        TextButton(
-            onPressed: () async {
-              if (fnamecontroller.text.isNotEmpty) {
-                showDialog(
-                  context: context,
-                  barrierDismissible: false, 
-                  builder: (BuildContext context) {
-                    return AlertDialog(
-                      content: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          CircularProgressIndicator(), 
-                          SizedBox(height: 20),
-                          Text("Cargando..."),
-                        ],
-                      ),
-                    );
-                  },
-                );
-                var datauser = await MongoDatabase.recuperarContrasenia(fnamecontroller.text);
-                Navigator.pop(context); 
-                if (datauser == true) {
-                  publicusername = fnamecontroller.text;
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => olvideContrasenia()),
-                  );
-                } else {
-                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("USUARIO NO EXISTE")));
-                             
-
-                }
-              }else{
-                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Ingresar Usuario")));
-
-              }
-            },
-            child: Text("Olvidé mi Contraseña"),
-          ),
-
-             SizedBox(height: 10,),
-            CheckboxListTile(
-          title: Text("¿Guardar Datos de Inicio de Sesion?",style: TextStyle(fontSize:13 ),),
-          value: checkedValue,
-          onChanged: (newValue) {
-          setState(() {
-           checkedValue = newValue!;
-          } );
+                  ],
+                ),
+              Text(
+                "Inserte Datos", 
+                style: TextStyle(fontSize: 22,fontFamily:"CourrierPrime"),
+                ),
+              
+                SizedBox(height: 50,
+                ),
+                Container(
+                  width: 300,
+                 decoration: BoxDecoration(
+                  color: Colors.grey,
+                  borderRadius: BorderRadius.circular(10),
+                ), 
+                  child: TextField(
+                    textAlign: TextAlign.center,
+                    controller: fnamecontroller,
+                    decoration: InputDecoration(labelText: "Nombre de Usuario"),
+                  ),
+                ),
+                SizedBox(height: 10,),
+             Stack(
+            alignment: Alignment.centerRight,  
+            children: [  
+             Container(
+              width: 300,
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.circular(10),
+              ),
+               child: TextField(
+                  textAlign: TextAlign.center,
+                  controller: contracontroller,
+                  obscureText: obscureText,
+                  decoration: InputDecoration(
+                    labelText: "Contraseña",
+                    errorText: showError ? "Contraseña o usuario incorrectos" : null,
+                  ),
+                  
+                  
+                ),
+             ),
+             IconButton(
+          onPressed: () {
+            setState(() {
+              obscureText = !obscureText;
+            });
           },
-          controlAffinity: ListTileControlAffinity.leading, 
+          icon: Icon(
+            obscureText ? Icons.visibility_off : Icons.visibility,
+            color: Colors.black,
           ),
-                      SizedBox(height: 50,),
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                //BOTON DE INGRESAR
-                OutlinedButton(
-  onPressed: () async {
-    if (fnamecontroller.text.isNotEmpty && contracontroller.text.isNotEmpty) {
-      if (kDebugMode) {
-        print("BOTON PRESIONADO INICIAR SESION");
-      }
-      showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return AlertDialog(
-                      content: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          CircularProgressIndicator(), 
-                          SizedBox(height: 20),
-                          Text("Cargando..."),
-                        ],
-                      ),
-                    );
-                  },
-                );
-      bool userExists = await MongoDatabase.getuser(fnamecontroller.text, contracontroller.text);
-
-      if (userExists) {
-        if (kDebugMode) {
-          print("ESTE USUARIO EXISTE");
-        }
-        publicusername = fnamecontroller.text;
-          if (checkedValue){
-            guardarValorPref();
-          }
-          Navigator.pop(context);
-            //  clearAll();
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(builder: (context) => PantallaInicio()),
-              );
-      } else {
-    Navigator.pop(context);
-
-        if (kDebugMode) {
-          print("CONTRASEÑA O USUARIO ERRONEO");
-        }
-        setState(() {
-               showError = true;
+              ),],),
+                SizedBox(height: 10,),
                 
-          } );
-        ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("CONTRASEÑA O USUARIO ERRONEO")));
-      }
-    }
-  },
-  child: Text("Iniciar Sesion")
-),
-
-      SizedBox(width: 20,),
-      OutlinedButton(
-  onPressed: () async {
-    nameappbar="Registro";
-    namebutton="Insertar";
-    set = 0;
-    Navigator.push(
-      context,
-      MaterialPageRoute(builder: (context) => RegistrarUsuario()),
-    );
-  },
-  child: Text("Registrarse")
-),
+                            Container(
+                              
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+              ),
+                              child: TextButton(
+                                              onPressed: () async {
+                                                if (fnamecontroller.text.isNotEmpty) {
+                                                  showDialog(
+                                                    context: context,
+                                                    barrierDismissible: false, 
+                                                    builder: (BuildContext context) {
+                                                      return AlertDialog(
+                                                        content: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                CircularProgressIndicator(), 
+                                SizedBox(height: 20),
+                                Text("Cargando..."),
+                              ],
+                                                        ),
+                                                      );
+                                                    },
+                                                  );
+                                                  var datauser = await MongoDatabase.recuperarContrasenia(fnamecontroller.text);
+                                                  Navigator.pop(context); 
+                                                  if (datauser == true) {
+                                                    publicusername = fnamecontroller.text;
+                                                    Navigator.push(
+                                                      context,
+                                                      MaterialPageRoute(builder: (context) => olvideContrasenia()),
+                                                    );
+                                                  } else {
+                                                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("USUARIO NO EXISTE")));
+                                   
+                                        
+                                                  }
+                                                }else{
+                                                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Ingresar Usuario")));
+                                        
+                                                }
+                                              },
+                                              child: Text("Olvidé mi Contraseña"),
+                                            ),
+                            ),
+          
+                 SizedBox(height: 10,),
+                Container(
+                  width: 300,
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.circular(10),
+              ),
+                  child: CheckboxListTile(
+                                title: Text("¿Guardar Datos de Inicio de Sesion?",style: TextStyle(fontSize:12 ),),
+                                value: checkedValue,
+                                onChanged: (newValue) {
+                                setState(() {
+                                 checkedValue = newValue!;
+                                } );
+                                },
+                                controlAffinity: ListTileControlAffinity.leading, 
+                                ),
+                ),
+                          SizedBox(height: 50,),
+          
+                Container(
+                  width: 280,
+                  height: 40,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+              ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      //BOTON DE INGRESAR
+                      OutlinedButton(
+                              onPressed: () async {
+                                if (fnamecontroller.text.isNotEmpty && contracontroller.text.isNotEmpty) {
+                            if (kDebugMode) {
+                              print("BOTON PRESIONADO INICIAR SESION");
+                            }
+                            showDialog(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return AlertDialog(
+                            content: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                CircularProgressIndicator(), 
+                                SizedBox(height: 20),
+                                Text("Cargando..."),
+                              ],
+                            ),
+                          );
+                        },
+                      );
+                            bool userExists = await MongoDatabase.getuser(fnamecontroller.text, contracontroller.text);
+                            
+                            if (userExists) {
+                              if (kDebugMode) {
+                                print("ESTE USUARIO EXISTE");
+                              }
+                              publicusername = fnamecontroller.text;
+                                if (checkedValue){
+                  guardarValorPref();
+                                }
+                                Navigator.pop(context);
+                  //  clearAll();
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => PantallaInicio()),
+                    );
+                            } else {
+                                Navigator.pop(context);
+                            
+                              if (kDebugMode) {
+                                print("CONTRASEÑA O USUARIO ERRONEO");
+                              }
+                              setState(() {
+                     showError = true;
+                      
+                                } );
+                              ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("CONTRASEÑA O USUARIO ERRONEO")));
+                            }
+                                }
+                              },
+                              child: Text("Iniciar Sesion")
+                            ),
+                            
+                            SizedBox(width: 20,),
+                            OutlinedButton(
+                              onPressed: () async {
+                                nameappbar="Registro";
+                                namebutton="Insertar";
+                                set = 0;
+                                Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => RegistrarUsuario()),
+                                );
+                              },
+                              child: Text("Registrarse")
+                            ),
+                    ],
+                  ),
+                ),
               ],
-            ),
-          ],
-        ),      
-       )
-      ),)
+            ),      
+           )
+          ),),
+        ],
+      )
         
     );
   }
